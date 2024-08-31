94ff3915e3d89b474db99abc0a2ae254198cf96a
Valadis2701 <124342932+Valadis2701@users.noreply.github.com>
2024-08-31 09:51:22 +0300

    Fixed youtube music

--- main.py
+++ main.py
@@ -19,15 +19,15 @@ TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logger.info(f"Пользователь {update.effective_user.id} запустил бота")
-    await update.message.reply_text('Привет! Я бот, который может скачивать контент с YouTube и TikTok. Просто отправь мне ссылку.')
+    await update.message.reply_text('Привет! Я бот, который может скачивать контент с YouTube, YouTube Music и TikTok. Просто отправь мне ссылку.')
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.message
     text = message.text
     logger.info(f"Получено сообщение от пользователя {update.effective_user.id}: {text}")
 
-    # Обновлённый паттерн для извлечения ссылок
-    url_pattern = r'(https?://(?:www\.)?(?:youtube\.com|youtu\.be|tiktok\.com|vm\.tiktok\.com|open\.spotify\.com)\S+)'
+    # Обновлённый паттерн для извлечения ссылок, включая YouTube Music
+    url_pattern = r'(https?://(?:www\.)?(?:youtube\.com|youtu\.be|music\.youtube\.com|tiktok\.com|vm\.tiktok\.com|open\.spotify\.com)\S+)'
     urls = re.findall(url_pattern, text)
 
     if urls:
@@ -51,7 +51,9 @@ async def download_and_send_media(update: Update, context: ContextTypes.DEFAULT_
     with tempfile.TemporaryDirectory() as temp_dir:
         logger.info(f"Создана временная директория: {temp_dir}")
         try:
-            if 'music.youtube.com' in url:
+            is_youtube_music = 'music.youtube.com' in url
+
+            if is_youtube_music:
                 ydl_opts = {
                     'format': 'bestaudio/best',
                     'postprocessors': [{
@@ -59,7 +61,7 @@ async def download_and_send_media(update: Update, context: ContextTypes.DEFAULT_
                         'preferredcodec': 'mp3',
                         'preferredquality': '192',
                     }],
-                    'outtmpl': os.path.join(temp_dir, f"{int(message.date.timestamp())}.%(ext)s"),
+                    'outtmpl': os.path.join(temp_dir, '%(title)s.%(ext)s'),
                 }
             else:
                 ydl_opts = {
@@ -72,6 +74,12 @@ async def download_and_send_media(update: Update, context: ContextTypes.DEFAULT_
                 info = ydl.extract_info(url, download=True)
                 filename = ydl.prepare_filename(info)
                 logger.info(f"Файл загружен: {filename}")
+
+            # Если файл был сконвертирован в mp3 для YouTube Music, обновляем имя файла
+            if is_youtube_music and filename.endswith(('.webm', '.m4a')):
+                new_filename = os.path.splitext(filename)[0] + '.mp3'
+                if os.path.exists(new_filename):
+                    filename = new_filename
 
             with open(filename, 'rb') as file:
                 if filename.endswith(('.mp4', '.webm')):
